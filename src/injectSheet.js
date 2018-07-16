c885dcefb0962410bc2abc0f6aa0a5eeb7013498
Pete Feltham <dev@felthy.com>
2018-07-16 20:34:02 +1000

    Log an error if used in production; better comments

--- src/injectSheet.js
+++ src/injectSheet.js
@@ -2,58 +2,63 @@ import defaultInjectSheet from 'react-jss'
 
 const managers = new WeakMap()
 
-export default process.env.NODE_ENV !== 'production'
-  ? function injectSheet(...rest) {
-    const createHoc = defaultInjectSheet.apply(this, rest)
-    return (InnerComponent) => {
-      const Jss = createHoc(InnerComponent)
+if (process.env.NODE_ENV === 'production') {
+  console.error('react-jss-hmr should never be used in production!')
+}
 
-      class HotJss extends Jss {
-        componentWillMount() {
-          // Note: this will never be called during hot module replacement, so we can
-          // use it as a place to store away the SheetManager
-          super.componentWillMount()
-          managers.set(Object.getPrototypeOf(this), this.manager)
-        }
+export default function injectSheet(...rest) {
+  const createHoc = defaultInjectSheet.apply(this, rest)
+  return (InnerComponent) => {
+    const Jss = createHoc(InnerComponent)
 
-        componentWillReceiveProps(nextProps, nextContext) {
-          super.componentWillReceiveProps(nextProps, nextContext)
+    class HotJss extends Jss {
+      componentWillMount() {
+        // Note: this will never be called during hot module replacement, so we can
+        // use it as a place to store away the SheetManager
+        super.componentWillMount()
+        managers.set(Object.getPrototypeOf(this), this.manager)
+      }
 
-          const prevManager = managers.get(Object.getPrototypeOf(this))
-          const manager = this.manager
-          if (prevManager !== manager) {
-            // If the manager changed between mounting and updating, it was probably due to hot
-            // module replacement.
-            // In that case, we need to recreate our sheets now. Then we can remove the old ones in
-            // componentDidUpdate().
-            const {theme} = this.state
-            const newState = this.createState({theme}, nextProps)
-            this.manage(newState)
-            this.setState(newState)
-          }
-        }
+      componentWillReceiveProps(nextProps, nextContext) {
+        super.componentWillReceiveProps(nextProps, nextContext)
 
-        componentDidUpdate(prevProps, prevState) {
-          const key = Object.getPrototypeOf(this)
-          const prevManager = managers.get(key)
-          const manager = this.manager
-          // If the manager changed between mounting and updating, it was probably due to hot module
-          // replacement.
-          // We remove previous sheets after new ones have been created to avoid FOUC.
-          const isHmrUpdate = prevManager && prevManager !== manager
-          if (isHmrUpdate) {
-            prevManager.unmanage(this.state.theme)
-            managers.set(key, manager)
-          }
-          if (!isHmrUpdate || prevState.dynamicSheet) {
-            // don't call when we don't have a previous dynamic sheet, otherwise it will throw if we
-            // added a new dynamic sheet via hmr
-            super.componentDidUpdate(prevProps, prevState)
-          }
+        const prevManager = managers.get(Object.getPrototypeOf(this))
+        const manager = this.manager
+        if (prevManager !== manager) {
+          // If the manager changed between mounting and updating, it was probably due to hot
+          // module replacement.
+          // In that case, we need to:
+          //   1. Call createState() again because although the constructor called it, RHL
+          //      restored the instance’s previous state so now the classes in the SheetsManager
+          //      do not match those in this.state
+          //   2. Call this.manage() so that the new SheetsManager will attach the new sheets
+          //   3. Call unmanage() on the old SheetsManager to clean up the old sheets
+          const {theme} = this.state
+          const newState = this.createState({theme}, nextProps)
+          this.manage(newState)
+          this.setState(newState)
+          // the new sheets have been attached now, so we can detach the old ones
+          prevManager.unmanage(theme)
         }
       }
 
-      return HotJss
+      componentDidUpdate(prevProps, prevState) {
+        const key = Object.getPrototypeOf(this)
+        const prevManager = managers.get(key)
+        const manager = this.manager
+
+        const isHmrUpdate = prevManager && prevManager !== manager
+        if (isHmrUpdate) {
+          managers.set(key, manager)
+        }
+        if (!isHmrUpdate || prevState.dynamicSheet) {
+          // don't call when we don't have a previous dynamic sheet, otherwise it will throw if we
+          // added a new dynamic sheet via hmr
+          super.componentDidUpdate(prevProps, prevState)
+        }
+      }
     }
+
+    return HotJss
   }
-  : defaultInjectSheet
+}
